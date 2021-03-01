853edbb8654b3d8f895103cc72e9a4076620225b
Kimjunkuk <54308434+Kimjunkuk@users.noreply.github.com>
2021-02-28 20:51:42 -0600

    Update UserDAO.java

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -8,53 +8,19 @@ import java.sql.SQLException;
 
 public class UserDAO {
 	
-	//09272020_private Connection conn;
-	//09272020_private PreparedStatement pstmt;
-	//09272020_private ResultSet rs;
-    //String driver = "org.mariadb.jdbc.Driver";
     Connection conn;
     PreparedStatement pstmt;
     ResultSet rs;
-	
-//	//������
-//	public UserDAO() {
-//		try {
-//			//67.167.223.168
-//			//Class.forName("org.mariadb.jdbc.Driver");
-//			Class.forName(driver);
-//			conn = DriverManager.getConnection(
-//					"jdbc:mariadb://localhost:3306/BBS",
-//					"mason",
-//					"Dosxmffj505@#"
-//					);
-//			//String dbURL ="jdbc:mariadb://localhost:3336/BBS";
-//			//?autoReconnect=true&amp;useSSL=false
-//			//String dbID = "root";
-//			//String dbPassword = "Dosxmffj505@#";
-//			
-//			
-//			//conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
-//		} catch (ClassNotFoundException e){
-//			System.out.println("����̹� �ε� ����");
-//			e.printStackTrace();
-//		} catch (SQLException e) {
-//			System.out.println("DB ���ӽ���");
-//			e.printStackTrace();
-//		}
-//		
+
 
 	 
 	public UserDAO() {
 		try {
 			
 			Class.forName("org.mariadb.jdbc.Driver");
-			String dbURL ="jdbc:mariadb://67.167.223.168:3306/bbs?autoReconnect=true&amp;useSSL=false";
-			//?autoReconnect=true&amp;useSSL=false
-			//?serverTimezone=Asia/Seoul&useSSL=false
-			//?autoReconnect=true&amp;useSSL=false
-			//DB�� �ߺ����� ������쿡�� �ش� ���� �߻��Ǵ°����� �Ǵܵ�
-			String dbID = "root";
-			String dbPassword = "Dosxmffj505@#";
+			String dbURL ="";
+			String dbID = "";
+			String dbPassword = "";
 			
 			
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
